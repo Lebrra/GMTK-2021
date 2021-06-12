7cc1524423a5c40a3962f31da7fd41f8548815f3
Leah Blasczyk <scout1665@gmail.com>
2021-06-12 13:52:21 -0500

    picking up and placing arm turrets

--- GMTK Project/Assets/Scripts/PlayerManager.cs
+++ GMTK Project/Assets/Scripts/PlayerManager.cs
@@ -7,18 +7,24 @@ public class PlayerManager : MonoBehaviour
     public bool active = true;
 
     PlayerMovement pm;
+    PlayerLimbFinder plf;
 
-    RobotAttack leftArm = null;
-    RobotAttack rightArm = null;
-    RobotAttack leftLeg = null;
-    RobotAttack rightLeg = null;
+    [SerializeField][Tooltip("LeftArm = 0\nRightArm = 1\nLeftLeg = 2\nRightLeg = 3")]
+    RobotAttack[] limbs = new RobotAttack[4];
+    public GameObject[] limbObjects;
 
     Rigidbody rb;
     Animator anim;
 
+    [Header("Limbs")]
+    public int activeLimbCount = 0;
+    public int activeLimb = 0;
+    bool scrollDelayer = false;
+
     void Start()
     {
         pm = GetComponent<PlayerMovement>();
+        plf = GetComponent<PlayerLimbFinder>();
 
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
@@ -29,11 +35,187 @@ public class PlayerManager : MonoBehaviour
         if (active)
         {
             pm.Move(rb, anim, transform);
+
+            if(activeLimbCount > 1 && !scrollDelayer)
+            {
+                if (Input.GetAxisRaw("Mouse ScrollWheel") + Input.GetAxisRaw("ScrollAlt") > 0)
+                {
+                    // increment active limb
+                    IncrementActiveLimb();
+                }
+                else if (Input.GetAxis("Mouse ScrollWheel") + Input.GetAxisRaw("ScrollAlt") < -0)
+                {
+                    // decrement active limb
+                    DecrementActiveLimb();
+                }
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                DetatchLimb();
+            }
+
+            if (Input.GetMouseButtonUp(1))
+            {
+                // check if attach is available
+                // attach
+                AttachLimb(plf.SelectTurret());
+            }
         }
     }
 
     public void AttachLimb(RobotAttack attack)
     {
+        if (activeLimbCount == 4)
+        {
+            Debug.LogWarning("limbs full, cannot add more");
+            return;
+        }
+        else if (!attack)
+        {
+            Debug.LogWarning("no turrets found");
+            return;
+        }
+        else
+        {
+            int slot = -1;
 
+            if (!attack.isLeg)
+            {
+                if (!limbs[0])
+                {
+                    // insert left arm
+                    slot = 0;
+                }
+                else if (!limbs[1])
+                {
+                    // insert right arm
+                    slot = 1;
+                }
+            }
+            else
+            {
+                if (!limbs[2])
+                {
+                    // insert left leg
+                    slot = 2;
+                }
+                else if (!limbs[3])
+                {
+                    // insert right leg
+                    slot = 3;
+                }
+            }
+
+            if (slot < 0)
+            {
+                Debug.Log("cannot pick up turret");
+                return;
+            }
+            else
+            {
+                // success, add component and remove turret
+                SetLimbComponent(slot, attack);
+                plf.RemoveTurret(attack);
+            }
+        }
+    }
+
+    void SetLimbComponent(int index, RobotAttack attack)
+    {
+        limbObjects[index].SetActive(true); // set different arm models here!
+
+        System.Type type = attack.GetType();
+        Component copy = limbObjects[index].AddComponent(type);
+        System.Reflection.FieldInfo[] fields = type.GetFields();
+        foreach (System.Reflection.FieldInfo field in fields)
+        {
+            field.SetValue(copy, field.GetValue(attack));
+        }
+
+        limbs[index] = copy as RobotAttack;
+
+        // final missing elements:
+        limbs[index].myTurretPrefab = attack.myTurretPrefab;
+        limbs[index].shootPosition = limbObjects[index].transform; // change to actual shoot position
+
+        activeLimbCount++;
+        activeLimb = index;
+    }
+
+    public void DetatchLimb()
+    {
+        if (activeLimb > -1 && activeLimb < 4)
+        {
+            //create new turret instance
+            GameObject newTurret = Instantiate(limbs[activeLimb].myTurretPrefab, transform.position + transform.forward.normalized * 2F, transform.rotation);
+            newTurret.GetComponent<RobotAttack>().SetTurret(limbs[activeLimb].health, limbs[activeLimb].myTurretPrefab);
+
+            //remove limb
+            Destroy(limbs[activeLimb]);
+            limbs[activeLimb] = null;
+            limbObjects[activeLimb].SetActive(false);
+            
+            activeLimbCount--;
+            if (activeLimbCount > 0) IncrementActiveLimb();
+            else activeLimb = -1;
+        }
+        else Debug.LogWarning("cannot detatch limb unfound");
+
+        Debug.Log("having this debug removes an error?");
+    }
+
+    void IncrementActiveLimb()
+    {
+        scrollDelayer = true;
+        StartCoroutine(ResetDelay());
+
+        activeLimb = (activeLimb + 1) % 4;
+
+        int iterator = 0;
+        while (limbs[activeLimb] == null)
+        {
+            activeLimb = (activeLimb + 1) % 4;
+            iterator++;
+
+            if (iterator >= 4)
+            {
+                Debug.LogWarning("Error Setting Limb");
+                activeLimb = -1;
+                return;
+            }
+        }
+
+        Debug.Log(activeLimb);
+    }
+
+    void DecrementActiveLimb()
+    {
+        scrollDelayer = true;
+        StartCoroutine(ResetDelay());
+
+        activeLimb--;
+        if (activeLimb < 0) activeLimb = 3;
+
+        int iterator = 0;
+        while (limbs[activeLimb] == null)
+        {
+            activeLimb--;
+            if (activeLimb < 0) activeLimb = 3;
+            iterator++;
+
+            if (iterator >= 4)
+            {
+                Debug.LogWarning("Error Setting Limb");
+                activeLimb = -1;
+                return;
+            }
+        }
+    }
+
+    IEnumerator ResetDelay()
+    {
+        yield return new WaitForSeconds(0.2F);
+        scrollDelayer = false;
     }
 }
