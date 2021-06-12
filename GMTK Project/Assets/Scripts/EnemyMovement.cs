bcbf2f4b73c64ed6af6cf114ff649e7911626ba8
WesMcW <wesmcwhorter@gmail.com>
2021-06-11 20:25:41 -0500

    Working on enemies

--- GMTK Project/Assets/Scripts/EnemyMovement.cs
+++ GMTK Project/Assets/Scripts/EnemyMovement.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    NavMeshAgent agent;
+    Vector3 destination;
+    public bool targetMet = false;
+
+    public Transform target;
+    public bool canMove;
+    public float attackRange;
+
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        destination = agent.destination;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    //Maybe add a Transform param
+    void Move()
+    {
+        if (canMove)
+        {
+            if (targetMet)
+            {
+                agent.SetDestination(transform.position);
+            }
+            else if (!targetMet)
+            {
+                destination = target.position;
+                agent.destination = destination;
+
+                if (Vector3.Distance(transform.position, destination) < attackRange)
+                {
+                    print("Yuh");
+                    targetMet = true;
+                }
+            }
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange);
     }
 }
