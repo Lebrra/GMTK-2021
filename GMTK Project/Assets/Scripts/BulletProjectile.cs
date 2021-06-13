13bbb2994a0e30d40caba6fd96a9191bbdd95eeb
squidmyers33 <squidmyers33@gmail.com>
2021-06-12 23:36:34 -0500

    goon foot dust

---
+++ GMTK Project/Assets/Scripts/BulletProjectile.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(speed != 0)
+        {
+            transform.position += transform.forward * (speed * Time.deltaTime);
+        }
+        else{
+            Debug.Log("Speed 0");
+        }
+    }
+
+}
